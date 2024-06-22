a8df688454c9e1feb402694f8cbab1cde4a5d44a
songsongyl <2444616264@qq.com>
2024-06-22 15:12:04 +0800

    experiment04

---
+++ src/main/java/com/example/controller/LoginServlet.java
@@ -0,0 +1,34 @@
+package com.example.controller;
+
+import com.example.entity.User;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        System.out.println("LoginServlet");
+        req.getRequestDispatcher("/WEB-INF/experiment05/jsp/login.html").forward(req,resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String userName = req.getParameter("userName");
+        String pwd = req.getParameter("password");
+        String url;
+        if(userName.equals("sy")&&pwd.equals("123")){
+            User user = new User("张三");
+            req.getSession().setAttribute("user",user);
+            url = "/welcome";
+        }else {
+            url = "/login";
+        }
+        resp.sendRedirect(req.getContextPath() + url);
+    }
+}
---
+++ src/main/java/com/example/entity/User.java
@@ -0,0 +1,23 @@
+package com.example.entity;
+
+public class User {
+    private String name;
+    private String account;
+    private String password;
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
